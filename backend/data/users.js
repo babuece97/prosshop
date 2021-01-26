dc2f0161d1171358fbc3b5422c17c31948182641
babuece97 <babuece97@yahoo.com>
2021-01-26 12:51:11 +0100

    3rd commit

---
+++ backend/data/users.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+
+    { 
+        name  : 'Adminn Userr',
+        email:'babuece97@yahoo.com',
+        password: bcrypt.hashSync('123456',10),
+        isAdmin :true
+    
+    },
+    { 
+        name  : 'babu',
+        password: bcrypt.hashSync('123456',10),
+        password: xxxx,
+    
+    },
+    { 
+        name  : 'shimu',
+        password: bcrypt.hashSync('123456',10),
+        password: xxxx,
+    
+    },
+]
+
+export default users
