03ca6addccf606b8b1ea7aebc369497d40a9162f
babuece97 <babuece97@yahoo.com>
2021-01-31 01:37:28 +0100

    7times

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from  './config/db.js'
 //import {connect } from 'mongoose'
-import products from './data/products.js'
+//import products from './data/products.js'
 //const products = require('./data/products')>>>>Normal JS syntex
+import productRoutes from './routes/productRoutes.js'
 const app =express()
 
 dotenv.config()
@@ -15,13 +16,7 @@ app.get('/', (req, res) => {
     res.send('API is running now...YES yes..')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
 const PORT = process.env.PORT || 5000
 
 app.listen(
